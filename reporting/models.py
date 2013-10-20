84cb9ef53d78d46c2d50f82bb3ce520c515a2f17
Daniel <daniel.pltk@gmail.com>
2013-10-20 15:55:42 +0300

    first View ("helloworld") is added

--- reporting/models.py
+++ reporting/models.py
@@ -3,9 +3,8 @@ from django.db import models
 class User(models.Model):
     full_name = models.CharField(max_length=300, null=True, blank=True)
     email = models.EmailField()
-    message = models.CharField(max_length=500)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-        return "Message from:" + self.email
+        return "User:" + self.full_name
             
