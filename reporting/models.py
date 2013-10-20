8653247897cdc73c9dda1c088af1847f9dce386f
Daniel <daniel.pltk@gmail.com>
2013-10-20 15:29:56 +0300

    admin config and first User model

--- reporting/models.py
+++ reporting/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# Creates your models here.
+class User(models.Model):
+    full_name = models.CharField(max_length=300, null=True, blank=True)
+    email = models.EmailField()
+    message = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __unicode__(self):
+        return "Message from:" + self.email
+            
