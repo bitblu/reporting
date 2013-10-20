ecec1c287121ccc3e5204101a1f3a43899c730b9
Daniel <daniel.pltk@gmail.com>
2013-10-20 16:53:45 +0300

    404.html added

--- reporting/views.py
+++ reporting/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from reporting.models import User
 
 # Main Template file for the login static view
 def index(request):
     return render(request, 'reporting/index.html')
+
+def detail(request, full_name):
+    name = get_object_or_404(User, pk=full_name)
+    return render(request, 'reporting/404.html')
