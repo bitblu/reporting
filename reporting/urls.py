cf25a3f7eac4145d04c83fe4f9cb640bc65db588
Daniel <daniel.pltk@gmail.com>
2013-10-20 16:18:22 +0300

    local reporting/urls.py was added

--- reporting/urls.py
+++ reporting/urls.py
@@ -8,11 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'reporting.views.index', name='index'),
-    url(r'^reporting/', include('reporting.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
